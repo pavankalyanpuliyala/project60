4364d989a2c42d87095b4d967e3e4c3d146887b2
pavankalyan <pavankalyanpuliyala@gmail.com>
2024-12-10 14:26:32 +0530

    this is sample roots java program

---
+++ roots.java
@@ -0,0 +1,10 @@
+public class roots {
+    public static void main(String[] args) {
+        // Predefined number
+        double number = 25.0; // This value is hardcoded
+
+        // Perform square root calculation
+        double result = Math.sqrt(number);
+
+        // Display the result
+        System.out.println("The square root of " + number + " is: " + result);}}
